58256ca7f4a8262daf83f0617784adc5f8c43778
ashishparihar679 <ashishparihar679@gmail.com.>
2026-09-20 01:17:49 +0530

    20SEP2026

---
+++ day3.js
@@ -0,0 +1,30 @@
+// DATA TYPE
+
+// let insti = "pyspiders"
+// console.log(insti)
+
+// // ! to find the data type of variable
+// console.log(typeof insti)
+// console.log(typeof (insti))
+
+// ! Number 
+let age = 20;
+// console.log(age)
+// console.log(typeof age)
+
+// ! BigInt : it is used to store the value that are too big
+let num = 123456789621483211456215n;
+// console.log(num,typeof(num))
+
+// ! boolean => (true / false)
+let isActive = true;
+// console.log(isActive,typeof(isActive))
+
+
+// ! null
+let b = null 
+console.log(b)
+console.log(typeof b)
+
+
+
